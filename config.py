966e54318dd6b5011700a2ae645418e15b64a7b7
Jin7156 <jinsoey482@gmail.com>
2026-05-08 00:06:16 +0900

    first

---
+++ config.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+clasroom = np.array([
+    [0,0,0, 1000,1000,1000]
+])
+
+eyes = []
+
+desk_lst = [
+    
+]
+
+stdnt_dict = {
+    "fhah":156,
+    "djed":234
+}
---
+++ fuctions.py
@@ -0,0 +1,13 @@
+import numpy as np
+import random as rm
+import config as cn
+
+#js에서 받은 값을 dick형태로 저장해야함, js에서 object형태로 저장하고 json으로 바꾼다음 json을 python의 dick으로 저장
+
+
+def ramdomset(dict):
+    names = list(dict.keys())
+    rm_namelst = rm.sample(names, len(names))
+    for i in range(len(names)):
+        cn.desk_lst[i].append(dict[rm_namelst[i]])
+    return rm_namelst #가장 효율적인 자리배치를 찾았을 경우의 리스트를 순차적으로 변수에 저장
